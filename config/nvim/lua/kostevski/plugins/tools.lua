a2e77b97f4ea155e618cef7d479d673bb688c107
a-kostevski <anton@kostevski.com>
2024-12-02 13:07:47 +0100

    Update gitignore

--- config/nvim/lua/kostevski/plugins/tools.lua
+++ config/nvim/lua/kostevski/plugins/tools.lua
@@ -1,6 +1,5 @@
 return {
    { "nvim-lua/plenary.nvim", lazy = true },
-   { "echasnovski/mini.comment", opts = {} },
    { "echasnovski/mini.icons", opts = {} },
    { import = "kostevski.plugins.tools" },
 }
