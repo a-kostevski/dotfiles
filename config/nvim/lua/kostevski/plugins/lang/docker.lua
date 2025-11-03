48d77581d8aa71dcfc35362edaef495601e2645a
a-kostevski <anton@kostevski.com>
2025-11-03 14:24:26 +0100

    Update docker lang spec in nvim

--- config/nvim/lua/kostevski/plugins/lang/docker.lua
+++ config/nvim/lua/kostevski/plugins/lang/docker.lua
@@ -1,59 +1,62 @@
 -- Root patterns for Docker projects
-require("kostevski.utils.root").add_patterns("dockerfile", {
-   -- Docker files
-   "Dockerfile",
-   "dockerfile",
-   "Dockerfile.*",
-   "*.dockerfile",
-   "*.Dockerfile",
-   -- Docker Compose
-   "docker-compose.yml",
-   "docker-compose.yaml",
-   "compose.yml",
-   "compose.yaml",
-   "docker-compose.*.yml",
-   "docker-compose.*.yaml",
-   -- Container configuration
-   ".dockerignore",
-   "docker-bake.json",
-   "docker-bake.hcl",
-   -- Build configurations
-   "buildspec.yml", -- AWS CodeBuild
-   ".gitlab-ci.yml", -- GitLab CI with Docker
-   "cloudbuild.yaml", -- Google Cloud Build
-   -- Kubernetes related
-   "k8s/",
-   "kubernetes/",
-   "helm/",
-   "charts/",
-   "skaffold.yaml",
-   -- Development
-   "devcontainer.json",
-   ".devcontainer/",
-})
+local lang = require("kostevski.utils.lang")
 
-return {
-   {
-      "mason",
-      opts = { ensure_installed = { "hadolint" } },
-   },
+return lang.register({
+  name = "Caddyfile",
+  root_markers = {
+    -- Docker files
+    "Dockerfile",
+    "dockerfile",
+    "Dockerfile.*",
+    "*.dockerfile",
+    "*.Dockerfile",
+    -- Docker Compose
+    "docker-compose.yml",
+    "docker-compose.yaml",
+    "compose.yml",
+    "compose.yaml",
+    "docker-compose.*.yml",
+    "docker-compose.*.yaml",
+    -- Container configuration
+    ".dockerignore",
+    "docker-bake.json",
+    "docker-bake.hcl",
+    -- Build configurations
+    "buildspec.yml", -- AWS CodeBuild
+    ".gitlab-ci.yml", -- GitLab CI with Docker
+    "cloudbuild.yaml", -- Google Cloud Build
+    -- Kubernetes related
+    "k8s/",
+    "kubernetes/",
+    "helm/",
+    "charts/",
+    "skaffold.yaml",
+    -- Development
+    "devcontainer.json",
+    ".devcontainer/",
+    ".git",
+  },
+  filetypes = { "Caddyfile" },
 
-   -- Linter Configuration
-   {
+  lsp_server = "gopls",
+  formatters = {
+    list = { "gofumpt", "goimports" },
+    tools = { "goimports", "gofumpt" },
+  },
+  linters = {
+    list = { "hadolint" },
+    tools = { "hadolint" },
+  },
+  additional_plugins = {
+    { "isobit/vim-caddyfile" },
+    {
       "mfussenegger/nvim-lint",
       opts = {
-         linters_by_ft = {
-            dockerfile = { "hadolint" },
-         },
+        linters_by_ft = {
+          dockerfile = { "hadolint" },
+        },
       },
-   },
-
-   -- Treesitter
-   {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-         opts.ensure_installed = opts.ensure_installed or {}
-         vim.list_extend(opts.ensure_installed, { "dockerfile" })
-      end,
-   },
-}
+    },
+  },
+  treesitter_parsers = { "dockerfile" },
+})
