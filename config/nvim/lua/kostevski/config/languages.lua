5f80b76fe20dcfa9298bedcdef72ab0f940776e4
a-kostevski <anton@kostevski.com>
2025-12-28 01:40:56 +0100

    (nvim) Add default language configuration file

---
+++ config/nvim/lua/kostevski/config/languages.lua
@@ -0,0 +1,15 @@
+-- Language Configuration
+-- Controls which language support modules are loaded
+--
+-- Options:
+--   enabled = "all"              -- Load all available languages
+--   enabled = { "lua", "go" }    -- Load only specified languages
+--
+--   overrides = {                -- Per-language configuration overrides
+--     python = { lsp_server = "pyright" },
+--   }
+
+return {
+  enabled = { "lua" },
+  overrides = {},
+}
